84453cc48bec8acc22be4bc035410cc4b987dbcf
Michael Hueschen <m@mhueschen.space>
2021-06-29 23:46:51 +0000

    more pospi progress

--- test/index.js
+++ test/index.js
@@ -17,3 +17,28 @@ const buildRunner = () => new Orchestrator({
 const getDNA = ((dnas) => (name) => (dnas[name]))({
   'scaffolding': path.resolve(__dirname, '../happs/scaffolding/scaffolding.dna'),
 })
+
+
+
+
+// main test script
+
+const runner = buildRunner()
+const config = Config.gen()
+
+runner.registerScenario('Basic DSL program compilation', async (scenario, t) => {
+  const [player] = await scenario.players([config])
+  const [[firstHapp]] = await player.installAgentsHapps(
+  [ // (don't quote me) agent key 1
+    [ // agent app bundle 1
+      [  // app bundle DNAs
+        getDNA('scaffolding'),
+      ]
+    ]
+  ])
+//  const appCellIds = firstHapp.cells.map(c => c.cellNick.match(/(\w+)\.dna$/)[1])
+  
+  const scaffoldingApp = firstHapp.cells[0]
+
+  scaffoldingApp.call('interpreter', 'test_output')
+})

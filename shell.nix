609b878b9a2aa3532669d32e39eadc9a59ad9420
Michael Hueschen <m@mhueschen.space>
2021-07-06 00:40:07 -0600

    copy syn default.nix to shell.nix

--- shell.nix
+++ shell.nix
@@ -1,8 +1,21 @@
 let
-  src = builtins.fetchGit {
-    url = "git@github.com:holochain/holonix";
-    rev = "014d28000c8ed021eb84000edfe260c22e90af8c";
+  holonixPath = builtins.fetchTarball {
+    url = "https://github.com/holochain/holonix/archive/3e94163765975f35f7d8ec509b33c3da52661bd1.tar.gz";
+    sha256 = "sha256:07sl281r29ygh54dxys1qpjvlvmnh7iv1ppf79fbki96dj9ip7d2";
   };
-in
+  holonix = import (holonixPath) {
+    includeHolochainBinaries = true;
+    holochainVersionId = "custom";
 
-import src {}
+    holochainVersion = {
+     rev = "363af6d8af8d18e4616f6aa56ad4d1f0fabaafb7";
+     sha256 = "sha256:0ssjhang6zljs0zrph998zj7582rf0vdb45p855awa7fmzpd4kfa";
+     cargoSha256 = "sha256:0y72lm5b0fl9anb2z9pcx1i3shqdlckz04zx3phc084hbzpig4cq";
+     bins = {
+       holochain = "holochain";
+       hc = "hc";
+     };
+    };
+    holochainOtherDepsNames = ["lair-keystore"];
+  };
+in holonix.main

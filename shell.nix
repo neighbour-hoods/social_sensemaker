4eef59c135c65533bee74a9294c01a11d379a7fa
Michael Hueschen <m@mhueschen.space>
2021-08-19 23:19:05 -0600

    update nix setup
    this combines some work from the holonix{,-2} branches, with a copy of
    the elemental-chat default.nix:
    
    https://github.com/holochain/elemental-chat/blob/d5abe1b381447cd276c1918170f0c16d06724801/default.nix
    
    we drop the use of `niv`, and thus can delete the whole `nix/` dir.

--- shell.nix
+++ shell.nix
@@ -1,14 +1,27 @@
 let
-  sources = import ./nix/sources.nix;
-  rust = import ./nix/rust.nix { inherit sources; };
-in
+  holonixPath = builtins.fetchTarball {
+    url = "https://github.com/holochain/holonix/archive/cedfc2453cfa795a0344acd6f5fb302362e18fc5.tar.gz";
+    sha256 = "sha256:004s7lkfhb5lg5292b80byx8b8zdm8lc3g0ldhfx9biqdg6m9agp";
+  };
+  holonix = import (holonixPath) {
+    includeHolochainBinaries = true;
+    holochainVersionId = "custom";
 
-{ pkgs ? import sources.nixpkgs {}
-}:
-
-pkgs.mkShell {
-  buildInputs = with pkgs; [
-    nodejs_latest
-    rust
+    holochainVersion = {
+     rev = "bd89d55e397baf7876099f600db997c89dd70fb6";
+     sha256 = "1nbq8qq4hzww5460khhv5ihj76bsnfqs306dcyknb2rq2firl1m1";
+     cargoSha256 = "sha256:1hbdcks7hkd3924wjy9qkizyn2hvhdvm14m23sjl9nn3xygj8w7w";
+     bins = {
+       holochain = "holochain";
+       hc = "hc";
+     };
+    };
+    holochainOtherDepsNames = ["lair-keystore"];
+  };
+  nixpkgs = holonix.pkgs;
+in nixpkgs.mkShell {
+  inputsFrom = [ holonix.main ];
+  buildInputs = with nixpkgs; [
+    binaryen
   ];
 }

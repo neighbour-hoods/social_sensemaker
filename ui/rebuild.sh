8a1148e4e209d540db13d570c83dcc3a3ae46e7c
Michael Hueschen <m@mhueschen.space>
2021-10-09 17:00:09 -0600

    switch to dev/debug builds in rebuild.sh

--- ui/rebuild.sh
+++ ui/rebuild.sh
@@ -1,3 +1,3 @@
 # must be run from containing directory
-CARGO_TARGET_DIR=$(pwd)/../target wasm-pack build $(pwd)/../crates/frontend --target web --out-dir $(pwd)/pkg
+CARGO_TARGET_DIR=$(pwd)/../target wasm-pack build --dev $(pwd)/../crates/frontend --target web --out-dir $(pwd)/pkg
 rollup ./main.js --format iife --file ./pkg/bundle.js

5273da24c6bc7a98591cc75729d0759417554ae9
Michael Hueschen <m@mhueschen.space>
2022-06-16 17:14:06 -0600

    `expand_remote_calls` macro: finished
    template in the body remote/bridge call.

--- crates/social_sensemaker_macros/src/lib.rs
+++ crates/social_sensemaker_macros/src/lib.rs
@@ -3,6 +3,9 @@
 use proc_macro::TokenStream;
 use proc_macro2::{Ident, Span};
 
+// TODO consolidate this / collapse other consts into this crate
+const SENSEMAKER_ZOME_NAME: &str = "sensemaker_main";
+
 // TODO think about hdk_extern and which zome/happ it goes into. will the widgets want
 // to invoke a macro, similar to `sensemaker_cell_id_fns`, s.t. the hdk_extern registers
 // in their wasm?
@@ -10,12 +13,14 @@ use proc_macro2::{Ident, Span};
 pub fn expand_remote_calls(_attrs: TokenStream, item: TokenStream) -> TokenStream {
     // expand_remote_calls is only valid for functions
     let item_fn = syn::parse_macro_input!(item as syn::ItemFn);
-    let fn_name = item_fn.sig.ident.clone();
+    let fn_name = item_fn.sig.ident.to_string();
 
     let mut new_fn = item_fn.clone();
 
+    // prefix fn ident.
     new_fn.sig.ident = Ident::new(&format!("remote_{}", fn_name), Span::call_site());
 
+    // arg list. tuple munging.
     {
         let arg_pat_type = match item_fn
             .sig
@@ -26,16 +31,39 @@ pub fn expand_remote_calls(_attrs: TokenStream, item: TokenStream) -> TokenStrea
             syn::FnArg::Typed(pat_type) => pat_type,
             _ => panic!("expand_remote_calls: invalid Receiver FnArg"),
         };
-        let arg_pat_type_pat = &arg_pat_type.pat;
         let arg_pat_type_ty = &arg_pat_type.ty;
         let token_stream = (quote::quote! {
-            (cell_id, cap_secret, #arg_pat_type_pat): (CellId, Option<CapSecret>, #arg_pat_type_ty)
+            (cell_id, cap_secret, payload): (CellId, Option<CapSecret>, #arg_pat_type_ty)
         })
         .into();
         let tup_arg = syn::parse_macro_input!(token_stream as syn::FnArg);
         assert!(new_fn.sig.inputs.pop().is_some());
         assert!(new_fn.sig.inputs.is_empty());
         new_fn.sig.inputs.push(tup_arg);
+    }
+
+    // body with bridge call.
+    {
+        let token_stream = (quote::quote! {
+            {
+                match call(
+                    CallTargetCell::Other(cell_id),
+                    #SENSEMAKER_ZOME_NAME.into(),
+                    #fn_name.into(),
+                    cap_secret,
+                    payload,
+                )? {
+                    ZomeCallResponse::Ok(response) => Ok(response.decode()?),
+                    err => {
+                        error!("ZomeCallResponse error: {:?}", err);
+                        Err(WasmError::Guest(format!("{}: {:?}", #fn_name, err)))
+                    }
+                }
+            }
+        })
+        .into();
+        let fn_body = syn::parse_macro_input!(token_stream as syn::Block);
+        new_fn.block = Box::new(fn_body);
     }
 
     (quote::quote! {
@@ -46,29 +74,4 @@ pub fn expand_remote_calls(_attrs: TokenStream, item: TokenStream) -> TokenStrea
         #new_fn
     })
     .into()
-
-    // steps
-    //
-    // 1. [x] clone ItemFn
-    // 2. [x] modify clone's fn name
-    // 3. [x] modify clone's fn sig, inserting new `cell_id: CellId` & `cap_secret: Option<CapSecret>` types onto a tuple whose 3rd value is the original arg.
-    // 3. [x] keep return type the same
-    // 4. [ ] declare/template `call(...)` fn body, which uses cell_id arg appropriately
-
-    // (quote::quote! {
-    //     match call(
-    //         CallTargetCell::Other($cell_id),
-    //         SENSEMAKER_ZOME_NAME.into(),
-    //         #fn_name.into(),
-    //         $cap_secret,
-    //         $payload,
-    //     )? {
-    //         ZomeCallResponse::Ok(response) => Ok(response.decode()?),
-    //         err => {
-    //             error!("ZomeCallResponse error: {:?}", err);
-    //             Err(WasmError::Guest(format!("#fn_name_remote: {:?}", err)))
-    //         }
-    //     }
-    // })
-    // .into()
 }

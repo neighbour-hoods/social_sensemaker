dcef3047925c2772d47bee98b422e938e71db54d
Michael Hueschen <m@mhueschen.space>
2022-08-26 15:55:05 -0600

    fix warnings

--- crates/sensemaker_ui/src/app.rs
+++ crates/sensemaker_ui/src/app.rs
@@ -4,7 +4,7 @@ use yew::prelude::*;
 
 use holochain_client_wrapper::{
     AdminWebsocket, AdminWsCmd, AdminWsCmdResponse, AppWebsocket, AppWsCmd, AppWsCmdResponse,
-    CellId, DeserializeFromJsObj, EntryHashRaw, EntryHeaderHashPairRaw, HashRoleProof,
+    CellId, DeserializeFromJsObj, HashRoleProof,
 };
 
 pub enum Msg {
@@ -41,7 +41,6 @@ impl Component for Model {
         let cell_id = CellId::deserialize_from_js_obj(props.cell_id_js.clone());
         let cell_id_ = cell_id.clone();
         let app_ws: AppWebsocket = props.app_ws_js.clone().into();
-        let app_ws_: AppWebsocket = app_ws.clone();
         let admin_ws: AdminWebsocket = props.admin_ws_js.clone().into();
         let admin_ws_ = admin_ws.clone();
         ctx.link().send_future(async move {
@@ -161,7 +160,7 @@ impl Component for Model {
         }
     }
 
-    fn view(&self, ctx: &Context<Self>) -> Html {
+    fn view(&self, _ctx: &Context<Self>) -> Html {
         html! {
             <div>
                 <p>{"hello, sensemaker 👋"}</p>
